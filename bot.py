8f82e920adaafa921b2867cb36173de22baf82ce
Kurt Dean <kmdean2016@gmail.com>
2020-03-03 13:39:18 -0500

    ping pong added

---
+++ bot.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='>')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+@bot.command()
+async def pong(ctx):
+    await ctx.send('ping')
+
+bot.run(process.env.BOT_TOKEN)
