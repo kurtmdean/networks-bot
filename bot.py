526568231b2bc7319d5e627294c69bff9d71ddfd
Kurt Dean <kmdean2016@gmail.com>
2020-03-03 15:40:07 -0500

    Command prefix set to !

--- bot.py
+++ bot.py
@@ -4,8 +4,15 @@ import discord
 import os
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='>')
+bot = commands.Bot(command_prefix='!')
 
+"""
+@bot.listen()
+async def on_message(message):
+    # on any new message, check the previous message in this channel.
+    # Increment the connection between these two people in the network
+        # If either of those people is not in the network, add them
+"""
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
